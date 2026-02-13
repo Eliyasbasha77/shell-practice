fa9d48962fe1c0ea236cb77ca0ffe0df9ee85e78
Eliyasbasha77 <eliyasbasha7794@gmail.com>
2026-02-13 16:38:33 +0530

    practice

---
+++ 30-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# No space before and after equal
+
+PERSON1=Suhail
+PERSON2=Adil
+
+echo "$PERSON1:: Hi Suresh How are you?"
+echo "$PERSON2: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: What are you learning recently?"
+echo "$PERSON2: I am learning Shell scripting $PERSON1"
