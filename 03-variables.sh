128a2418f60e6ea41dedc5197ab092473c6026e7
Eliyasbasha77 <eliyasbasha7794@gmail.com>
2026-02-13 17:06:09 +0530

    practice

--- 03-variables.sh
+++ 03-variables.sh
@@ -8,3 +8,13 @@ echo "$PERSON1:: Hi Suresh How are you?"
 echo "$PERSON2: Hello $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: What are you learning recently?"
 echo "$PERSON2: I am learning Shell scripting $PERSON1"
+
+
+add=$((3+2))
+sub=$((7-2))
+mult=$((8*3))
+div=$((8/2))
+echo "$add"
+echo "$sub"
+echo "$mult"
+echo "$div"
