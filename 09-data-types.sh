edec0a1e7d82ce758b978565c95d4b3a5096a699
Eliyasbasha77 <eliyasbasha7794@gmail.com>
2026-02-16 14:06:38 +0530

    practice

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -8,3 +8,9 @@ SUM=$(($NUMBER1+$NUMBER2+$NAME))
 
 echo "$SUM"
 
+TOOLS=("Linux" "Shell" "Docker" "Kubernetes")
+
+# size = 4; max index = 3 
+echo "All tools: ${TOOLS[@]}"
+echo "First tool: ${TOOLS[0]}"
+
