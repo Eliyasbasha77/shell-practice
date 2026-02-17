c96ce73e8152531230ce4f1dc6309707da11ad78
Eliyasbasha77 <eliyasbasha7794@gmail.com>
2026-02-17 13:07:03 +0530

    practice

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -8,3 +8,13 @@ if [ $(($NUMBER % 2)) -eq 0 ]; then
 else
     echo "given number $NUMBER is ODD"
 fi       
+
+
+echo "Please enter the number"
+read NUMBER
+
+if [ $(($NUMBER)) -lt 20 ]; then
+    echo "given number $NUMBER is Prime Number"
+else
+    echo "given number $NUMBER is NOt a Prime NUMBER"
+fi       
