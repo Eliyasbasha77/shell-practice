54cacd423ff0d4af9f76c89fb465ae51bd5ffd43
Eliyasbasha77 <eliyasbasha7794@gmail.com>
2026-02-23 13:01:24 +0530

    practice

--- 16-while-loop.sh
+++ 16-while-loop.sh
@@ -1,16 +1,16 @@
-!/bin/bash
+# !/bin/bash
 
-count=5
+# count=5
 
-    echo "Starting countdown..."
-while [ $count -gt 0 ]
-do
-    echo "Time left: $count"
-    sleep 1 #pause for 1 sec
-    count=$((count -1)) #Decrement the count
-done
+#     echo "Starting countdown..."
+# while [ $count -gt 0 ]
+# do
+#     echo "Time left: $count"
+#     sleep 1 #pause for 1 sec
+#     count=$((count -1)) #Decrement the count
+# done
 
-echo "Times up!"
+# echo "Times up!"
 
 while IFS= read -r line; do
     # Process each line here
